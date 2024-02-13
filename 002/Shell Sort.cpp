8eaa8320360f3eab22f94c64c4a3e75a8cd63520
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-02-13 23:56:45 +0500

    Add files via upload

---
+++ 002/Shell Sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    int n = 8;
+    int arr[] = {3, 6, 8, 2, 1, 7, 4, 5};
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    // Output the sorted array
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
