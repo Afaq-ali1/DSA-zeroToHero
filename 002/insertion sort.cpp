ca2ccf594cda084fcb24d433349f51eb5a871a3e
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-02-14 09:12:16 +0500

    Update insertion sort.cpp

--- 002/insertion sort.cpp
+++ 002/insertion sort.cpp
@@ -1,17 +1,48 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+class Method
+{
+	public:	
+	void shellSort(int arra[],int n)
+	{
+		int phase=1;
+		for(int gap=n/2;gap>0;gap/=2)
+		{
+			cout<<"  	Phase numer: "<<phase<<endl;
+			cout<<"	gap is "<<gap<<endl;
+			for(int i=gap;i<n;i++)
+			{
+				int min = arra[i];
+				int j=i-gap;
+				while(j>=0&&arra[j]>min)
+				{
+					arra[j+gap]=arra[j];
+					cout<<"["<<j<<"]"<<"-->"<<"["<<j+gap<<"]"<<endl;
+					j-=gap;
+				}
+				arra[j+gap]=min;
+			}
+			phase++;
+		}
+	}
+	void display(int arra[],int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			cout<<arra[i]<<" ";
+		}
+		cout<<endl;
+	}
+};
 int main()
 {
-  int n=5;
-  int arr[n]=[1,2,3,4,5];
-  for(int i;i<n;i++)
-  int min=i;
-  int j=n+1;
-  while(j<=0 && arr[j]>arr[min])
-  {
-    arr[min]=arr[j];
-    j--;
-  }
-  arr[min]=arr[j];
-  arr[j]=arr[j-1];
+	Method m;
+	int arr[] ={7,6,9,4,5,6,6,7,3,1};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout<<"Array is:"<<endl;
+	m.display(arr,n);
+	m.shellSort(arr,n);
+	cout<<endl;
+	cout<<"Sorted Array is:"<<endl;
+	m.display(arr,n);
 }
