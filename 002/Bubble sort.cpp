1a04225fc60ae8921b7f509110d7a681029f43e1
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-02-19 13:59:14 +0500

    Update Bubble sort.cpp

--- 002/Bubble sort.cpp
+++ 002/Bubble sort.cpp
@@ -2,16 +2,14 @@
 using namespace std;
 class Sort{
 	public:
-		int arr[5] = {64, 25, 12, 22, 11};
-		int n=sizeof(arr)/sizeof(arr[0]);
 		bool swap;
-	void selectonSort()
+	void bubbleSort(int arr,int size)
 	{
 		int outer=0,inner=0;
-		for(int i=0;i<n-1;i++)
+		for(int i=0;i<size-1;i++)
 		{
 			swap=false;
-			for(int j=0;j<n-i-1;j++)
+			for(int j=0;j<size-i-1;j++)
 			{
 				if(arr[j]>arr[j+1])
 				{		
@@ -32,22 +30,26 @@ class Sort{
 			cout<<"After sorting:"<<endl;
 		for(int i=0;i<n;i++)
 		{
-			cout<<arr[i]<<endl;
+			cout<<arr[i]<<" ";
 		}
+		cout<<endl;
 	}
 	
-	void display()
+	void display(int arr,int size)
 	{
-		for(int i=0;i<n;i++)
+		for(int i=0;i<size;i++)
 		{
-			cout<<arr[i]<<endl;
+			cout<<arr[i]<<" ";
 		}	
 	}
 };
 int main()
 {
+	
+	int array[5] = {64, 25, 12, 22, 11};
+	int n=sizeof(arr)/sizeof(arr[0]);
 	Sort s;
 	cout<<"Before sorting:"<<endl;
-	s.display();
-	s.selectonSort();
+	s.display(array,n);
+	s.bubbleSort(array,n);
 }
