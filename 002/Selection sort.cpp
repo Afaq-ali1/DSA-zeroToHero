5223acba09a99a6c4c5d489337b63ece844fea5a
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-03-11 11:48:22 +0500

    Update Selection sort.cpp

--- 002/Selection sort.cpp
+++ 002/Selection sort.cpp
@@ -24,7 +24,7 @@ class Sort{
 			
 		for(int i=0;i<n;i++)
 		{
-			cout<<arr[i]<<endl;
+			cout<<arr[i]<<" ";
 		}
 	}
 	
@@ -32,7 +32,7 @@ class Sort{
 	{
 		for(int i=0;i<n;i++)
 		{
-			cout<<arr[i]<<endl;
+			cout<<arr[i]<<" ";
 		}	
 	}
 };
