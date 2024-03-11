9d6fc9940475dee4cec8cd01d146325c89e6fe57
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-03-11 11:35:00 +0500

    Update Binary Search.cpp

--- 003/Binary Search.cpp
+++ 003/Binary Search.cpp
@@ -3,34 +3,39 @@ using namespace std;
 class Method
 {
 	public:	
-	int binarySearch(int arra[],int size,int target )
-	{
-		int left =0;
-		int right=size-1;
-		int mid= left+(right-left)/2;
-		while(left<=right)
-		{
-			if(arra[mid]==target)
-			{
-				return mid;
-			}
-			else if(target>arra[mid])
-  			{
-  				left=mid+1;
-			}
-			else
-  			{
-  				right=mid-1;
-			}
-			return -1;	
-		}
-	}
+	int binarySearch(int sortedArray[],int n,int target) 
+{
+    int low = 0;                                                                            // O(1)
+    int high = n - 1;
+
+    while (low <= high) // This loop runs log(n) times, where n is the size of the Sorted Array
+    {
+        int mid = low + (high - low) / 2;                                       // O(1)
+        cout<<"low: "<<low<<endl;
+        cout<<"high: "<<high<<endl;
+        	cout<<"mid is "<<mid<<endl;
+        if (sortedArray[mid] == target)                                        // O(1)
+        {
+        	cout<<"run if"<<endl;
+            return mid;                                                                    // O(1)
+        } 
+        else if (sortedArray[mid] < target)                                   // O(1)
+        {        	
+        	cout<<"run else if"<<endl;
+            low = mid + 1;           
+		} 
+        else {   high = mid - 1;}                                                    // O(2)
+    }
+    return -1; // This operation is O(1)
+}
+
+
 };
 int main()
 {
 	Method m;
-	int arr[] ={1,2,3,4,5,6,7,8,9,10};
+	int arr[] ={11,12,13,14};
 	int n = sizeof(arr)/sizeof(arr[0]);
-		int t=5;
-		cout<<m.binarySearch(arr,n,t);
+		int targetNumber=13;
+		cout<<"found at Index: "<< m.binarySearch(arr,n,targetNumber);
 }
