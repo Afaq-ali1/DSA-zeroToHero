ec3e2353b9640ee1841d236b54229d217d62dee0
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-03-29 23:46:43 +0500

    Add files via upload

---
+++ LinkList/Nodes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+struct Node{
+		int data;
+		Node* nextNode;
+};
+int main()
+{
+	Node n3;
+	n3.data=3;
+	n3.nextNode=NULL;
+	
+	Node n2;
+	n2.data=2;
+	n2.nextNode=&n3;
+	
+	Node n1;
+	n1.data=1;
+	n1.nextNode=&n2;
+	Node* curNode= &n1;
+}
