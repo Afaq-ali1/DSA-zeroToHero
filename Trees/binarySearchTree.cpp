ba85cd5ac721cd5da4c0dc7bd532153e3d149bc2
Afaq Ali <133626948+Afaq-ali1@users.noreply.github.com>
2024-05-03 23:32:43 +0500

    Update binarySearchTree.cpp

--- Trees/binarySearchTree.cpp
+++ Trees/binarySearchTree.cpp
@@ -43,7 +43,62 @@ class BST{
 			}
 		}
 	}
-	void display(Node* current)
+	
+	void deleteNode(Node* root,int key)
+	{
+		while(root!=NULL&&root->data!=key)
+		{
+			if(key < root->data)
+			{
+				root=root->left;	
+			}
+			else
+			{
+				root=root->right;
+			}
+		}
+		if(root==NULL)
+		{
+			return;
+		}	
+		if(root->right==NULL&&root->left==NULL)
+		{
+			delete root;
+			root=NULL;
+		}
+		else if(root->right==NULL)
+		{
+			Node* temp=root;
+			root=root->left;
+			delete temp;
+		}
+		
+		if(root->left==NULL)
+		{
+			Node* temp=root;
+			root=root->right;
+			delete temp;
+		}
+		else
+		{
+			Node* temp=root->right;
+			while(temp->left!=NULL)
+			{
+				temp=temp->left;
+			}
+			root->data=temp->data;
+			if(!temp->right)
+			{
+				delete temp;
+			}
+			else
+			{
+				temp=temp->right;
+			}
+		}
+		
+	}
+		void display(Node* current)
 	{
 		if(current)
 		{
@@ -52,13 +107,16 @@ class BST{
 			display(current->right);
 		}
 	}
+
 };
 int main()
 {
 	BST bst;
-	bst.insertNode(12);
-	bst.insertNode(32);
+	bst.insertNode(22);
+	bst.insertNode(23);
 	bst.insertNode(21);
-	bst.insertNode(52);
+	bst.insertNode(20);
+	bst.deleteNode(bst.root,20);
 	bst.display(bst.root);
+
 }
